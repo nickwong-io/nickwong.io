d784b905cf328fdb36d1eb01377f9289909b7a5d
Nick Wong <nick@nickwong.io>
2019-06-07 00:02:27 -1000

    fix aloha friday picture thing and update copy

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,17 @@
 $(document).foundation();
 
-console.log('runnin')
+var d = new Date();
+var n = d.getDay();
+
+if (n === 5) {
+	console.log('Aloha Friday!');
+
+	//image styles
+	$('.hero_image').css("background-image", "url(\"assets/images/nicksGrad_compressed.jpg\")");
+	$('.hero_image').css("background-position", "50% 35%");
+	$('#small-hero-image').attr("src", "assets/images/nicksGrad_compressed.jpg");
+
+	// header styles
+	$('#alohaFriday').css("display", "unset")
+	$('#reg').css("display", "none")
+}
