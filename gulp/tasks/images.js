cd61ca5c5985192efaad7481e3c228da0fa1d8b9
Nick Wong <nick@nickwong.io>
2019-06-28 23:55:09 -1000

    add resume

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,6 +2,6 @@ var gulp              = require('gulp'),
     paths             = require('../config').paths;
 
 gulp.task('images', function() {
-  gulp.src(paths.src.images + "/**/*.{gif,jpg,jpeg,png,svg}")
+  gulp.src(paths.src.images + "/**/*.{gif,jpg,jpeg,png,svg,pdf}")
     .pipe(gulp.dest(paths.dist.images));
 });
