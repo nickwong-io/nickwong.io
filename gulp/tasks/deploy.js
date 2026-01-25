daaf70254a89888d18165c565e71171008ba85d7
niwong <nick@nickwong.io>
2026-01-24 20:43:49 -0800

    Update git deployment strategy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,9 +1,21 @@
-// deploy to gh-pages
-var gulp    = require('gulp'),
-    ghPages = require ('gulp-gh-pages');
+// deploy to gh-pages using git directly
+var gulp = require('gulp');
+var exec = require('child_process').exec;
 
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*').pipe(ghPages({
-    branch: "gh-pages"
-  }));
+var commands = [
+  'cd dist',
+  'rm -rf .git',
+  'git init',
+  'git add -A',
+  'git commit -m "Deploy"',
+  'git push -f git@github.com:nickwong-io/nickwong.io.git HEAD:gh-pages',
+  'rm -rf .git'
+].join(' && ');
+
+gulp.task('deploy', function(done) {
+  exec(commands, { maxBuffer: 1024 * 1024 }, function(err, stdout, stderr) {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    done(err);
+  });
 });
